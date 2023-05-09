7b3532d433affadae479b5bbbcb06e81d92bc1f3
Limbert Lino <limbertlino@gmail.com>
2023-05-09 17:48:57 -0400

    Done Exercise 1.7 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-// button has text and handle click
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
@@ -13,14 +12,29 @@ const Statistics = ({ text, state }) => {
   );
 };
 
+const Total = ({ total }) => {
+  return <p>All {total}</p>;
+};
+
+const Average = ({ total }) => {
+  return <p>Average {total / 3}</p>;
+};
+
+const PositivePercentage = ({ good, total }) => {
+  return <p>Positive {(good / total) * 100}</p>;
+};
+
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  console.log({ good, neutral, bad });
+  console.log((good / total) * 100);
+
   const handleClick = (state, updateState) => {
     updateState(state + 1);
+    setTotal(total + 1);
   };
 
   return (
@@ -36,6 +50,12 @@ function App() {
       <Statistics text="good" state={good} />
       <Statistics text="neutral" state={neutral} />
       <Statistics text="bad" state={bad} />
+
+      <Total total={total} />
+
+      <Average total={total} />
+
+      <PositivePercentage good={good} total={total} />
     </div>
   );
 }
