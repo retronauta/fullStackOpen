d5a03a3d13c31c55163a8ed4ecd10ed5a910f687
Limbert Lino <limbertlino@gmail.com>
2023-05-11 16:05:33 -0400

    Done exercise 2.8 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,33 +1,45 @@
 import { useState } from "react";
 
+const DisplayNumbers = ({ persons }) => {
+  return (
+    <>
+      {persons.map(person => (
+        <Person name={person.name} number={person.number} key={person.name} />
+      ))}
+    </>
+  );
+};
+
+const Person = ({ name, number }) => {
+  return (
+    <p>
+      {name} {number}
+    </p>
+  );
+};
+
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
-
-  const DisplayNumbers = ({ persons }) => {
-    return (
-      <>
-        {persons.map(person => (
-          <Person name={person.name} key={person.name} />
-        ))}
-      </>
-    );
-  };
-
-  const Person = ({ name }) => {
-    return <p>{name}</p>;
-  };
+  const [newNumber, setNewNumber] = useState("");
 
   const handleName = event => {
     setNewName(event.target.value);
+  };
+
+  const handleNumber = event => {
+    setNewNumber(event.target.value);
   };
 
   const addNewPerson = event => {
     event.preventDefault();
     findDuplicate(persons, newName) >= 0
       ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat({ name: newName }));
+      : setPersons(persons.concat({ name: newName, number: newNumber }));
     setNewName("");
+    setNewNumber("");
   };
 
   const findDuplicate = (arr, item) =>
@@ -37,9 +49,11 @@ function App() {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNewPerson}>
-        {/* <div>debug: {newName}</div> */}
         <div>
           name: <input value={newName} onChange={handleName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumber} />
         </div>
         <div>
           <button type="submit">add</button>
