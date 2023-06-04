3ac6ba1d12296675c1716cc0876e266438041ae9
Limbert Lino <limbertlino@gmail.com>
2023-06-04 09:39:11 -0400

    Done exercise 4.2 blog list

---
+++ part4/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const config = require('./utils/config');
+const mongoose = require('mongoose');
+const blogRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(response => logger.info('connected to MONGODB'))
+  .catch(error => logger.error('Error connecting to database', error.message));
+
+app.use(cors());
+app.use(express.json());
+
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+module.exports = app;
