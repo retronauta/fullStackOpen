62ffc3478dd80ddbdf3d33b14e928288f6e9d85e
Limbert Lino <limbertlino@gmail.com>
2023-06-11 09:23:37 -0400

    Done exercise 4.8 blog list

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,14 +1,13 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogRouter.get('/', (request, response, next) => {
-  Blog.find({})
-    .then(blogs => response.json(blogs))
-    .catch(error => next(error));
+blogRouter.get('/', async (req, res, next) => {
+  const blogs = await Blog.find({});
+  res.json(blogs);
 });
 
-blogRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body);
+blogRouter.post('/', (req, res, next) => {
+  const blog = new Blog(req.body);
 
   blog
     .save()
