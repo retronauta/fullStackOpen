8cb8fbed9133669f131307d4dc5a4e6c3d26b2e3
Limbert Lino <limbertlino@gmail.com>
2023-06-14 21:44:55 -0400

    Done exercise 4.15 blog list

--- part4/app.js
+++ part4/app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const blogRouter = require('./controllers/blogs');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
+const userRouter = require('./controllers/users');
 
 mongoose.set('strictQuery', false);
 
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
